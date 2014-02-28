4f16069c2acabe8085df8f8fcefa95d842efb949
kamalnayan <info.kndeo@gmail.com>
2014-02-28 14:55:05 +0530

    Update index.js
    again

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ var app = {
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
-            document.getElementById("result1").value= result.text;
+            document.getElementById("txtName").value= result.text;
             if(result.text!="")
             {
             alert("Scanner result: \n" +
